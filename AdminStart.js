f322ac4de908b19651c49762dd0acd49fba5801e
Nastya <anastasia.vorton@gmail.com>
2017-04-10 20:50:23 +0300

    Query 2 results

--- AdminStart.js
+++ AdminStart.js
@@ -15,33 +15,54 @@ $("#query1").on("click", function () {
 $("#loadQuery1").on("click", function () {
     $.ajax({
         type: "POST",
-        url: "/load/query1/"+document.getElementById("query1Year").value,
+        url: "/load/query1/" + document.getElementById("query1Year").value,
         success: function (result) {
             query1output(result);
         }
     });
 });
 
-var wrapper = $(".query1-additional");
+var wrapper1 = $(".query1-additional");
+var wrapper2 = $(".query2-additional");
 
 function query1output(input) {
     var temp = input.split("&");
-    var text =  "";
+    var text = "";
     for (var i = 0; i < temp.length; i++) {
         text += '<h3>' + temp[i] + '<br>';
     }
-    $(wrapper).html(text);
+    $(wrapper1).html(text);
 }
 
+$("#query2").on("click", function () {
+    $.ajax({
+        type: "POST",
+        url: "Query2.html",
+        success: function (html) {
+            $("#container-main-field").html(html);
+        }
+    });
+});
 
-//
-// $("#js-back-button").on("click", function () {
-//     saveFieldsPage2();
-//     $.ajax({
-//         type: "POST",
-//         url: "Page1.html",
-//         success: function (html) {
-//             $("#container-main-field").html(html);
-//         }
-//     });
-// });
+$("#loadQuery2").on("click", function () {
+    $.ajax({
+        type: "POST",
+        url: "/load/query2/" + document.getElementById("query2Unit").value + "&" + document.getElementById("query2StartYear").value + "&" + document.getElementById("query2FinalYear"),
+        success: function (result) {
+            query2output(result);
+        }
+    });
+});
+
+function query2output(input) {
+    var temp = input.split("&");
+    for (var i = 0; i < temp.length; i++) {
+        var out = "";
+        var text = temp[i].split("/s/");
+        for (var j = 0; j < text.length; j++) {
+            out += '<h3>' + temp[i] + '<br>';
+        }
+        out += '<br>'
+    }
+    $(wrapper2).html(out);
+}
