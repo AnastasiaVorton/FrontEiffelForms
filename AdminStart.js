3689ce981b0f4abe89e6db995f7a72882102c5be
Nastya <anastasia.vorton@gmail.com>
2017-04-10 18:54:58 +0300

    Submit button

---
+++ AdminStart.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Nastya on 10.04.2017.
+ */
+$("#query1").on("click", function () {
+    $.ajax({
+        type: "POST",
+        url: "Query1.html",
+        success: function (html) {
+            $("#container-main-field").html(html);
+        }
+    });
+});
+
+
+$("#loadQuery1").on("click", function () {
+    alert(document.getElementById("query1Year").value);
+    $.ajax({
+        type: "POST",
+        url: "/load/query1/"+document.getElementById("query1Year").value,
+        success: function (result) {
+            query1output(result);
+        }
+    });
+});
+
+var wrapper = $(".query1-additional");
+
+function query1output(input) {
+    var temp = input.split("&");
+    for (var i = 0; i < temp.length; i++) {
+        $(wrapper).append('<p>' + temp[i] + '<br>');
+    }
+}
+
+
+//
+// $("#js-back-button").on("click", function () {
+//     saveFieldsPage2();
+//     $.ajax({
+//         type: "POST",
+//         url: "Page1.html",
+//         success: function (html) {
+//             $("#container-main-field").html(html);
+//         }
+//     });
+// });
