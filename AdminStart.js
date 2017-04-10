e564c6ca811478dce3acddcbfb784a84f9f2cb45
Nastya <anastasia.vorton@gmail.com>
2017-04-10 21:07:54 +0300

    Query 2 results

--- AdminStart.js
+++ AdminStart.js
@@ -24,6 +24,7 @@ $("#loadQuery1").on("click", function () {
 
 var wrapper1 = $(".query1-additional");
 var wrapper2 = $(".query2-additional");
+var wrapper3 = $(".query3-additional");
 
 function query1output(input) {
     var temp = input.split("&");
@@ -60,9 +61,42 @@ function query2output(input) {
         var out = "";
         var text = temp[i].split("/s/");
         for (var j = 0; j < text.length; j++) {
-            out += '<h3>' + temp[i] + '<br>';
+            out += '<h3>' + text[j] + '<br>';
         }
         out += '<br>'
     }
     $(wrapper2).html(out);
+}
+
+$("#query3").on("click", function () {
+    $.ajax({
+        type: "POST",
+        url: "Query3.html",
+        success: function (html) {
+            $("#container-main-field").html(html);
+        }
+    });
+});
+
+$("#loadQuery3").on("click", function () {
+    $.ajax({
+        type: "POST",
+        url: "/load/query3/" + document.getElementById("query3Lab").value + "&" + document.getElementById("query3StartYear").value + "&" + document.getElementById("query3StartSem").value + "&" + document.getElementById("query3FinalYear").value + "&" + document.getElementById("query3FinalSem").value,
+        success: function (result) {
+            query3output(result);
+        }
+    });
+});
+
+function query3output(input) {
+    var temp = input.split("&");
+    for (var i = 0; i < temp.length; i++) {
+        var out = "";
+        var text = temp[i].split("/s/");
+        for (var j = 0; j < text.length; j++) {
+            out += '<h3>' + text[j] + '<br>';
+        }
+        out += '<br>'
+    }
+    $(wrapper3).html(out);
 }
